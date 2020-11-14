b8e7a593c8e386c8fe2b10edd9010ca4ff24b751
Nisarg Shah <nisargcse@gmail.com>
2020-11-14 10:25:16 -0600

    Vanilla Bellmanford method work
    Geeks for Geeks references included, still need to add stable GUI and each step display

--- dvr.py
+++ dvr.py
@@ -2,6 +2,59 @@
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 
+
+class Graph:
+
+    def __init__(self, vertices):
+        self.V = vertices # No. of vertices
+        self.graph = []
+
+    # function to add an edge to graph
+    def addEdge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    # utility function used to print the solution
+    def printArr(self, dist):
+        print("Vertex Distance from Source")
+        for i in range(1,self.V):
+            print("{0}\t\t{1}".format(i, dist[i]))
+
+    # The main function that finds shortest distances from src to
+    # all other vertices using Bellman-Ford algorithm. The function
+    # also detects negative weight cycle
+    def BellmanFord(self, src):
+
+        # Step 1: Initialize distances from src to all other vertices
+        # as INFINITE
+        dist = [float("Inf")] * self.V
+        dist[src] = 0
+
+
+        # Step 2: Relax all edges |V| - 1 times. A simple shortest
+        # path from src to any other vertex can have at-most |V| - 1
+        # edges
+        for _ in range(self.V -1):
+            # Update dist value and parent index of the adjacent vertices of
+            # the picked vertex. Consider only those vertices which are still in
+            # queue
+            for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                        dist[v] = dist[u] + w
+
+        # Step 3: check for negative-weight cycles. The above step
+        # guarantees shortest distances if graph doesn't contain
+        # negative weight cycle. If we get a shorter path, then there
+        # is a cycle.
+
+        for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                        print("Graph contains negative weight cycle")
+                        return
+
+        # print all distance
+        self.printArr(dist)
+
+
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 f = open(filename, 'r')
@@ -9,10 +62,17 @@ Lines = f.readlines()
 
 data = [lines.split() for lines in Lines]
 
+g = Graph(len(data))
 
 
 
+g = Graph(len(data))
+for d in data:
+    g.addEdge(int(d[0]),int(d[1]),int(d[2]))
+    g.addEdge(int(d[1]),int(d[0]),int(d[2]))
 
+
+g.BellmanFord(1)
 
 
 
@@ -23,4 +83,5 @@ References :
 
 https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
 
+https://www.geeksforgeeks.org/bellman-ford-algorithm-dp-23/
 """
